656876e4694083144faf15ba4f6771bd240b10f4
Bensaid Massyl <ex.trouppers@gmail.com>
2022-01-11 16:34:09 +0100

    A

--- shutter.py
+++ shutter.py
@@ -150,8 +150,8 @@ def on_message_old(client, userdata, msg):
 
 def on_message(client, userdata, msg):
     ''' process incoming message. WARNING: threaded environment! '''
+    payload = json.loads(msg.payload.decode('utf-8'))
     print("Received message '" + json.dumps(payload) + "' on topic '" + msg.topic + "' with QoS " + str(msg.qos))
-    #payload = json.loads(msg.payload.decode('utf-8'))
     #if(payload['topic'] == MQTT_SUB):
     #    if(payload['value']=='UP'):
     #        print('UP')
